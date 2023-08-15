c1299ee8e160a3ad5e59b470f10e73d02ed71a2b
David Val <101377109+Val-senseisama@users.noreply.github.com>
2023-08-15 13:11:34 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,9 @@ const __dirname1 = path.resolve();
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname1, "/frontend/a-townhall/build")));
 
-    app.get("*", express.static(path.join(__dirname1, "/frontend/a-townhall/build", "index.html"));
-    );
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname1, "/frontend/a-townhall/build", "index.html"));
+    });
 } else {
     app.get("/", (req, res) => {
         res.send("API up and running away");
