8cb3468c34daf0f261d21db5d9885a6c1643e58b
David Val <101377109+Val-senseisama@users.noreply.github.com>
2023-08-15 13:00:15 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -63,9 +63,7 @@ io.on("connection", ( socket ) => {
         if(!chat.users) return console.log("chat.users not defined");
 
         chat.users.forEach(user => {
-            if(user._id == newMessageReceived.sender._id) return;
-
-            socket.in(user._id).emit("message received", newMessageReceived);
+            if(user._id == newMessageReceived.sender._id) return socket.in(user._id).emit("message received", newMessageReceived);
         });
     });
 
