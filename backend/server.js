744596a98909f8a34223cab0bb7e12ca7212c051
David Val <101377109+Val-senseisama@users.noreply.github.com>
2023-08-15 13:20:18 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ if(process.env.NODE_ENV === "production") {
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname1, "/frontend/a-townhall/build", "index.html"));
     });
+    app.use("*", express.static(path.join(__dirname1, "/frontend/a-townhall/build")));
 } else {
     app.get("/", (req, res) => {
         res.send("API up and running away");
