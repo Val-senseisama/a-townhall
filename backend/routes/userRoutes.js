e5b98add99bc7304bdd55fcd4c117dd4ae744529
Val <valizevbigiedavid@gmail.com>
2023-08-16 22:20:19 +0100

    69

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const { registerUser, updateBg } = require("../controllers/userControllers");
-const { authUser }= require("../controllers/userControllers");
+const { authUser } = require("../controllers/userControllers");
 const { allUsers } = require("../controllers/userControllers");
-const { protect } = require("../middleware/authMiddleware")
-
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").post(registerUser).get(protect, allUsers);
-router.route("/updatebackground").put(protect, updateBg)
-router.post("/login", authUser); yy 
+router.route("/updatebackground").put(protect, updateBg);
+router.post("/login", authUser);
 
-
-module.exports=router;
+module.exports = router;
